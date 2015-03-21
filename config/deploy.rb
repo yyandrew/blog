89bbcaa32d9d15f221ad6542536b472cf3cb8114
Andrew Yang <zzcv20051122@gmail.com>
2015-03-21 11:28:03 +0800

    MISC - change deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
+set :application, 'blog'
 set :repo_url, 'git@github.com:andrew222/blog.git'
 
 # Default branch is :master
@@ -45,4 +45,5 @@ namespace :deploy do
     end
   end
 
+  after :deploy, "deploy:restart"
 end
